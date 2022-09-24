32d2bc96d5b04cb5e9d117c170baebf428e42230
zlx <>
2022-09-24 14:53:19 +0800

    添加 dist 目录

---
+++ dist/OpenCVBuilder.js
@@ -0,0 +1,22 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+require('./utils-9d03e134.js');
+require('fs');
+require('npmlog');
+var OpenCVBuilder = require('./OpenCVBuilder-d4856ac9.js');
+require('child_process');
+require('os');
+require('path');
+require('picocolors');
+require('tiny-glob');
+require('rimraf');
+require('util');
+require('crypto');
+require('tiny-glob/sync');
+
+
+
+exports.OpenCVBuilder = OpenCVBuilder.OpenCVBuilder;
+exports["default"] = OpenCVBuilder.OpenCVBuilder;
---
+++ dist/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var OpenCVBuilder = require('./OpenCVBuilder-d4856ac9.js');
+require('./utils-9d03e134.js');
+require('child_process');
+require('fs');
+require('os');
+require('path');
+require('npmlog');
+require('picocolors');
+require('tiny-glob');
+require('rimraf');
+require('util');
+require('crypto');
+require('tiny-glob/sync');
+
+
+
+exports.ALLARGS = OpenCVBuilder.ALLARGS;
+exports.ALL_OPENCV_MODULES = OpenCVBuilder.ALL_OPENCV_MODULES;
+exports.OpenCVBuildEnv = OpenCVBuilder.OpenCVBuildEnv;
+exports.OpenCVBuilder = OpenCVBuilder.OpenCVBuilder;
+exports.args2Option = OpenCVBuilder.args2Option;
+exports["default"] = OpenCVBuilder.OpenCVBuilder;
+exports.genHelp = OpenCVBuilder.genHelp;
+exports.getLibsFactory = OpenCVBuilder.getLibsFactory;
